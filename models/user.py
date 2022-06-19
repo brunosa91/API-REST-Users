30d001136bcae4c00d772dc7213cfd78c7f3b970
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-19 18:49:14 -0300

    realizado os métodos responsáveis por fazer as operações no banco de dados

--- models/user.py
+++ models/user.py
@@ -23,4 +23,19 @@ class UserModel(db.model):
            'telefone':self.telefone
        }
 
-    def 
+    @classmethod
+    def find_by_id(cls,id):
+        return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def save_to_db(self, ):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self, ):
+        db.session.delete(self)
+        db.session.commit()
+        
