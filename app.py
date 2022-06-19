685e60f100cceac84e611f32c9d5e8701222e576
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-19 18:11:03 -0300

    importado o server e pego a instancia e api no arquivo app

--- app.py
+++ app.py
@@ -1,34 +1,11 @@
-from flask import Flask, jsonify
+from flask import jsonify
+from marshmallow import ValidationError
+from ma import ma
+from db import db   
 
-user = [
-    {
-        'name':'Bruno Santana de Sá',
-        'email':'brunosantana@gmail.com',
-        'telefone':'(11)988887777'
-    }
-]
+from server.instance import server
 
-app =  Flask(__name__)
-
-@app.route('/add_user', methods=['POST'])
-def createUser():
-    pass
+api = server.api
+app = server.app
 
 
-@app.route('/users' , methods=['GET'])
-def getUser():
-    return jsonify(user)
-
-@app.route('/users/<id>' , methods=['GET'])
-def getUserById(id):
-    pass
-
-@app.route('/edit_user/<id>' , methods=['PUT'])
-def editUserById(id):
-    pass
-
-@app.route('/delete_user/<id>' , methods=['DELETE'])
-def deleteUserById(id):
-    pass
-
-app.run(port=5000)
