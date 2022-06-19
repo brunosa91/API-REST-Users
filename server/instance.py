3b0bc661c9aea4480ef19cde27bbb52170eafd43
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-19 16:08:47 -0300

    Criado as instance e feita as configurações do servidor

---
+++ server/instance.py
@@ -0,0 +1,22 @@
+from flask import Flask, Blueprint
+from flask_restful import Api
+
+class Server():
+    def __init__(self,):
+        self.app = Flask(__name__)
+        self.blueprint = Blueprint('api',__name__,url_prefix='/api')
+        self.api= Api(self.blueprint,doc='/doc', title='Sample Flask-SQLAlchemy')
+        self.app.register_blueprint(self.blueprint)
+
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        self.app.config['PROPAGATE_EXCEPTIONS'] = True
+        
+        def user_ns(self, ):
+            return self.api.namespace(name='User', description='user related operations', path='/')
+        def run(self, ):
+            self.app.run(
+                port=5000,
+                debug=True,
+                host='0.0.0.0'
+        )
