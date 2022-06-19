704e7a4af593f5611d9856f5281ce85a38a5c8e7
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-19 18:20:37 -0300

    criado a função para criar as tabelas no banco de dados

--- app.py
+++ app.py
@@ -8,4 +8,12 @@ from server.instance import server
 api = server.api
 app = server.app
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+if __name__ = '__main__':
+    db.init_app(app)
+    ma.init_app(app)
+    server.run()
 
