4d183b90073a9c212f47cf3094f08b905c848a1b
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-19 11:14:44 -0300

    criado as rotas  para executar o CRUD

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import Flask
 
 app =  Flask(__name__)
 
-@app.route('/')
-def home():
-    return "testando rota"
+@app.route('/add_user', methods=['POST'])
+def createUser():
+    pass
+
+
+@app.route('/users' , methods=['GET'])
+def getUser():
+    pass
+
+@app.route('/users/<id>' , methods=['GET'])
+def getUserById(id):
+    pass
+
+@app.route('/edit_user/<id>' , methods=['PUT'])
+def editUserById(id):
+    pass
+
+@app.route('/delete_user/<id>' , methods=['DELETE'])
+def deleteUserById(id):
+    pass
 
 app.run(port=5000)
