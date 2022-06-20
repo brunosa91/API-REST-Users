bc7e8d91e481f77d54808c6d59a8891dc895478e
brunosa91 <brunosantana.engenharia@gmail.com>
2022-06-20 08:50:05 -0300

    Importado a controller e implementado os endpoints

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import jsonify
-from marshmallow import ValidationError
+from flask import Flask, Blueprint, jsonify
+from flask_restx import Api
 from ma import ma
-from db import db   
+from db import db
+from marshmallow import ValidationError
+from controllers.user_controller import User, UserList, UserAdd,UserDelete,UserEdit
 
 from server.instance import server
 
@@ -12,7 +14,13 @@ app = server.app
 def create_tables():
     db.create_all()
 
-if __name__ = '__main__':
+api.add_resource(User,'/users/<int:id>')
+api.add_resource(UserList,'/users')
+api.add_resource(UserAdd,'/add_users')
+api.add_resource(UserDelete,'/delete_user/<int:id>')
+api.add_resource(UserEdit,'/edit_user/<int:id>')
+
+if __name__ == '__main__':
     db.init_app(app)
     ma.init_app(app)
     server.run()
